1859aae3cc025d862b881dbc23a2e5b5e38e4b1d
Steffen Schneider <steffen@bethgelab.org>
2022-04-12 17:30:12 +0200

    Move benchmark files to DeepLabCut

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,34 +1,7 @@
-"""Generate the benchmark table"""
+import deeplabcut.benchmark
+import deeplabcut.benchmark.cli
 
-import argparse
-
-import benchmark
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--include", nargs="+", default=None, required=False)
-    parser.add_argument(
-        "--onerror", 
-        default="return", 
-        required=False, 
-        choices=("ignore", "return", "raise")
-    )
-    parser.add_argument("--nocache", action="store_true")
-    return parser.parse_args()
-
+import benchmark.submissions
 
 if __name__ == "__main__":
-    args = parse_args()
-    if not args.nocache:
-        results = benchmark.loadcache()
-    else:
-        results = None
-    results = benchmark.evaluate(
-        include_benchmarks=args.include,
-        results=results,
-        on_error=args.onerror,
-    )
-    if not args.nocache:
-        benchmark.savecache(results)
-    print(results.toframe())
+    deeplabcut.benchmark.cli.main()
