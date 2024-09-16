413b8e582e1aee8044174bbb01d699358582fd22
Niels Poulsen <niels.poulsen@epfl.ch>
2024-09-16 11:59:21 +0200

    fix APT benchmark results - add random score

--- benchmark/submissions/apt/predict_apt.py
+++ benchmark/submissions/apt/predict_apt.py
@@ -1,8 +1,14 @@
-from deeplabcut import benchmark
-from deeplabcut.benchmark.benchmarks import TriMouseBenchmark, MarmosetBenchmark, ParentingMouseBenchmark, FishBenchmark 
 import json
 
-code="https://github.com/kristinbranson/APT.git"
+import numpy as np
+
+from deeplabcut import benchmark
+from deeplabcut.benchmark.benchmarks import TriMouseBenchmark, MarmosetBenchmark, ParentingMouseBenchmark, FishBenchmark 
+
+
+code = "https://github.com/kristinbranson/APT.git"
+data_root = "benchmark/submissions/apt/data"
+
 
 def get_names():
   return "GRONe", "MMPose-CiD", "DeTR+GRONe", "DeTR+Hrformer"
@@ -19,34 +25,73 @@ def get_prefix(name):
 
   return prefix
 
+
+def parse_predictions(data: dict) -> dict:
+  """Sets the data into the expected format for the DeepLabCut benchmark
+
+  Not needed if the "score"/"pose" keys are added to the JSON files
+  """
+  rng = np.random.default_rng(0)
+
+  predictions = {}
+  for img, poses in data.items():
+    img_predictions = []
+
+    # give first prediction base score 0.95, second 0.9, third 0.85, ...
+    score_steps = np.linspace(0.95, 0, 20)
+
+    # so that all scores are different and sorting is deterministic
+    score_offset = rng.random(1) / 50
+
+    for i, pose in enumerate(poses):
+      # get random score
+      idv_score = np.clip(score_steps[i] + score_offset, 0, 1).item()
+
+      img_predictions.append(
+        {
+          "pose": pose,
+          "score": idv_score,
+        }
+      )
+    predictions[img] = tuple(img_predictions)
+
+  return predictions
+
+
 @benchmark.register
 class APTTriMouse(TriMouseBenchmark):
+  code = code
+
   def names(self):
     return get_names()
 
   def get_predictions(self,name):
     prefix = get_prefix(name)
 
-    res_file = f'data/{prefix}_trimice_test.json'
+    res_file = f'{data_root}/{prefix}_trimice_test.json'
     with open(res_file) as f:
-      return json.load(f)
+      return parse_predictions(json.load(f))
 
 
 @benchmark.register
 class APTMarmoset(MarmosetBenchmark):
+  code = code
+
   def names(self):
     return get_names()
 
   def get_predictions(self,name):
     prefix = get_prefix(name)
 
-    res_file = f'data/{prefix}_marmoset_test.json'
+    res_file = f'{data_root}/{prefix}_marmoset_test.json'
     with open(res_file) as f:
-      return json.load(f)
+      return parse_predictions(json.load(f))
 
 
 @benchmark.register
 class APTParenting(ParentingMouseBenchmark):
+  code = code
+
   def names(self):
     return get_names()
     #return "GRONe", "DeTR+GRONe", "DeTR+Hrformer"
@@ -54,21 +99,23 @@ class APTParenting(ParentingMouseBenchmark):
   def get_predictions(self,name):
     prefix = get_prefix(name)
 
-    res_file = f'data/{prefix}_parenting_test.json'
+    res_file = f'{data_root}/{prefix}_parenting_test.json'
     with open(res_file) as f:
-      return json.load(f)
+      return parse_predictions(json.load(f))
 
 
 @benchmark.register
 class APTFish(FishBenchmark):
+  code = code
+
   def names(self):
     return get_names()
 
   def get_predictions(self,name):
     prefix = get_prefix(name)
 
-    res_file = f'data/{prefix}_fish_test.json'
+    res_file = f'{data_root}/{prefix}_fish_test.json'
     with open(res_file) as f:
-      return json.load(f)
+      return parse_predictions(json.load(f))
 
 
